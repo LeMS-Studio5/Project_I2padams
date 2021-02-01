c89d50164ebafb62849df6c142c84d1bbf4c19d4
Luke S <Luke@LUKE-ACER>
2021-02-01 17:26:44 -0500

    Added btn from ProChic and added new method for creating alpha texture

---
+++ libProChic/Button.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace libProChic
+{
+
+    public class Button : System.Windows.Forms.Button
+    {
+        // Implements IButtonControl
+        private Color colLightBorder;
+        private Color colDarkBorder;
+        private bool blnPressed = false;
+        private bool blnHeld = false;
+        private static MasterClass com = new MasterClass();
+        public Button() : base()
+        {
+            try
+            {
+                BackColor = com.convertColour(com.Config.GetConfig("Colors", "ButtonFace").Setting);
+                DoubleBuffered = true;
+                // colLightBorder = ControlPaint.Light(BackColor, 50)
+                // colDarkBorder = ControlPaint.Dark(BackColor, 50)
+                base.Paint += OnPaint;
+                base.MouseUp += Button_MouseUp;
+                base.MouseDown += Button_MouseDown;
+                base.MouseClick += Button_Click;
+                Invalidate();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        private void Button_MouseDown(object sender, MouseEventArgs e)
+        {
+            blnPressed = (e.Button == MouseButtons.Left);
+            Invalidate();
+            MouseDown?.Invoke(this, e);
+        }
+        private void Button_MouseUp(object sender, MouseEventArgs e)
+        {
+            if ((blnPressed && HoldOnPush && !Held))
+                Held = true;
+            if (blnPressed && (e.Button == MouseButtons.Left))
+                blnPressed = false;
+            Invalidate();
+            MouseUp?.Invoke(this, e);
+        }
+        private void Button_Click(object sender, MouseEventArgs e)
+        {
+            if (blnPressed && (e.Button == MouseButtons.Left))
+                blnPressed = false;
+            Invalidate();
+            Click(sender, new EventArgs());
+            MouseClick(sender, e);
+           // MouseClick?.Invoke(this, e);
+        }
+        private  void OnPaint(object sender, PaintEventArgs e)
+        {
+            // keep       '   MyBase.OnPaint(e)
+            // BackColor = com.convertColour(com.Config.GetConfig("Colors", "ButtonFace"))
+            // keep
+            // keep
+            // keep       ForeColor = com.convertColour(com.Config.GetConfig("Colors", "ButtonText"))
+            // Console.WriteLine(Font.Name & Font.Size)
+            // Dim f As New Font()
+            // keep       'Font = SaveSystem.currentTheme.buttonFont
+            // keep
+            // keep
+            colLightBorder = ControlPaint.Light(BackColor, 50);
+            colDarkBorder = ControlPaint.Dark(BackColor, 50);
+            // keep
+            Graphics g = e.Graphics;
+            g.Clear(BackColor);
+            // keep
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+            StringFormat sf = new StringFormat();
+            sf.Alignment = StringAlignment.Center;
+            sf.LineAlignment = StringAlignment.Center;
+
+            sf.HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show;
+            // ImageAli
+            if ((blnPressed && Enabled) || Held)
+            {
+                g.FillRectangle(new SolidBrush(colLightBorder), new Rectangle(0, 0, Width, Height));
+                g.FillRectangle(Brushes.Black, new Rectangle(0, 0, Width - 1, Height - 1));
+                g.FillRectangle(new SolidBrush(colDarkBorder), new Rectangle(1, 1, Width - 2, Height - 2));
+                if (Held)
+                    g.FillRectangle( new TextureBrush(AlphaTexture()),2,2, Width - 3, Height - 3);
+                else
+                    g.FillRectangle(new SolidBrush(BackColor), new Rectangle(2, 2, Width - 3, Height - 3));
+                if (! (Image==null))
+                    g.DrawImage(Image, Convert.ToInt32((Width / (double)2) - (Image.Width / (double)2)), Convert.ToInt32((Height / (double)2) - Convert.ToInt32(Image.Height / (double)2)));
+                g.DrawString(Text, Font, new SolidBrush(ForeColor), new Rectangle(2, 2, Width - 3, Height - 3), sf);
+            }
+            else
+            {
+                g.FillRectangle(Brushes.Black, new Rectangle(0, 0, Width, Height));
+                g.FillRectangle(new SolidBrush(colLightBorder), new Rectangle(0, 0, Width - 1, Height - 1));
+                g.FillRectangle(new SolidBrush(colDarkBorder), new Rectangle(1, 1, Width - 2, Height - 2));
+                g.FillRectangle(new SolidBrush(BackColor), new Rectangle(1, 1, Width - 3, Height - 3));
+                if (!(Image == null))
+                    g.DrawImage(Image, Convert.ToInt32((Width / (double)2) - (Image.Width / (double)2)), Convert.ToInt32((Height / (double)2) - Convert.ToInt32(Image.Height / (double)2)));
+                if ((Enabled))
+                    g.DrawString(Text, Font, new SolidBrush(ForeColor), new Rectangle(1, 1, Width - 3, Height - 3), sf);
+                else
+                    g.DrawString(Text, Font, new SolidBrush(colDarkBorder), new Rectangle(1, 1, Width - 3, Height - 3), sf);
+            }
+            if ((ShowFocusRectangle && Enabled && Focused))
+                g.DrawRectangle(new Pen(Color.Black, 1) { DashPattern = new[] { 1f, 1f } }, new Rectangle(3, 3, Width - 7, Height - 7));
+        }
+        public Bitmap AlphaTexture()
+        {
+            Bitmap bmp = new Bitmap(4, 4);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawRectangle(new Pen(Color.Silver), 1, 0, 1, 1);
+            g.DrawRectangle(new Pen(Color.Silver), 0, 1, 1, 1);
+            return bmp;
+        }
+        public new FlatStyle FlatStyle
+        {
+            set
+            {
+            }
+            get
+            {
+                return FlatStyle.Flat;
+            }
+        }
+        public new TextImageRelation TextImageRelation { get; } = TextImageRelation.Overlay;
+        public bool HoldOnPush { get; set; } = false;
+        public bool ShowFocusRectangle { get; set; } = true;
+        public bool Held
+        {
+            set
+            {
+                blnHeld = value;
+                Invalidate();
+                if (value == true)
+                {
+                    foreach (Control con in Parent.Controls)
+                    {
+                        if (con is Button)
+                        {
+                            if (!((Button)con).Name.Equals(Name))
+                                ((Button)con).Held = false;
+                        }
+                    }
+                }
+            }
+            get
+            {
+                return blnHeld;
+            }
+        }
+        public new event MouseUpEventHandler MouseUp;
+
+        public  delegate void MouseUpEventHandler(object sender, MouseEventArgs e);
+
+        public new  event MouseClickEventHandler MouseClick = new MouseClickEventHandler((e, a) => { });
+
+        public  delegate void MouseClickEventHandler(object sender, MouseEventArgs e);
+
+        public new  event MouseDownEventHandler MouseDown;
+
+        public  delegate void MouseDownEventHandler(object sender, MouseEventArgs e);
+
+        public new event EventHandler Click = new EventHandler((e, a) => { });
+        
+    }
+}
