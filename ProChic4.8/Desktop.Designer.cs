66d2285cd78d380f171b3eb2b4af3ce25d853017
Luke S <Luke@LUKE-ACER>
2021-02-01 11:14:10 -0500

    Added Support for AutoUpdate on Desktop Added Taskbar panel

--- ProChic4.8/Desktop.Designer.cs
+++ ProChic4.8/Desktop.Designer.cs
@@ -28,34 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            libProChic.ImageList imageList1 = new libProChic.ImageList();
+            libProChic.ImageAlbum imageAlbum1 = new libProChic.ImageAlbum();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Desktop));
+            libProChic.ImageList imageList2 = new libProChic.ImageList();
+            libProChic.ImageAlbum imageAlbum2 = new libProChic.ImageAlbum();
             this.elvDesktop = new libProChic.ExplorerListView();
+            this.panTaskBar = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // elvDesktop
             // 
-            this.elvDesktop.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.elvDesktop.AutoRefreshFolder = false;
+            this.elvDesktop.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.elvDesktop.AutoRefreshFolder = true;
             this.elvDesktop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.elvDesktop.Directory = "C:\\Users\\Luke\\Desktop\\";
+            this.elvDesktop.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.elvDesktop.Directory = "";
             this.elvDesktop.DisplayMode = libProChic.ExplorerListView.DisplayType.DirectoriesAndFiles;
+            this.elvDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
             this.elvDesktop.FileSizeType = libProChic.ExplorerListView.SizeType.Bytes;
             this.elvDesktop.Filter = "*.*";
-            this.elvDesktop.FollowPallet = false;
+            this.elvDesktop.FollowPallet = true;
             this.elvDesktop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.elvDesktop.GridLines = true;
+            imageList1.Images = imageAlbum1;
+            this.elvDesktop.LargeImageList = imageList1;
             this.elvDesktop.Location = new System.Drawing.Point(0, 0);
             this.elvDesktop.Name = "elvDesktop";
             this.elvDesktop.OnErrorGoToParentDirectory = false;
+            this.elvDesktop.OSIconLocationPath = "";
             this.elvDesktop.OwnerDraw = true;
+            this.elvDesktop.Pattern = ((System.Drawing.Bitmap)(resources.GetObject("elvDesktop.Pattern")));
             this.elvDesktop.Root = "C:\\";
-            this.elvDesktop.SelectedColor = System.Drawing.Color.Empty;
-            this.elvDesktop.SelectedTextColor = System.Drawing.Color.Empty;
-            this.elvDesktop.Size = new System.Drawing.Size(640, 435);
+            this.elvDesktop.SelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(128)))));
+            this.elvDesktop.SelectedTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.elvDesktop.Size = new System.Drawing.Size(640, 480);
+            imageList2.Images = imageAlbum2;
+            this.elvDesktop.SmallImageList = imageList2;
             this.elvDesktop.TabIndex = 0;
+            this.elvDesktop.UpdateDesktop = false;
             this.elvDesktop.UseCompatibleStateImageBehavior = false;
+            this.elvDesktop.View = System.Windows.Forms.View.SmallIcon;
             this.elvDesktop.ViewType = libProChic.ExplorerListView.ExplorerType.Desktop;
+            this.elvDesktop.Wallpaper = ((System.Drawing.Bitmap)(resources.GetObject("elvDesktop.Wallpaper")));
+            this.elvDesktop.WallpaperLayout = System.Windows.Forms.ImageLayout.None;
+            // 
+            // panTaskBar
+            // 
+            this.panTaskBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panTaskBar.Location = new System.Drawing.Point(0, 380);
+            this.panTaskBar.Name = "panTaskBar";
+            this.panTaskBar.Size = new System.Drawing.Size(640, 100);
+            this.panTaskBar.TabIndex = 1;
             // 
             // Desktop
             // 
@@ -63,6 +87,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(640, 480);
             this.ControlBox = false;
+            this.Controls.Add(this.panTaskBar);
             this.Controls.Add(this.elvDesktop);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Desktop";
@@ -75,6 +100,7 @@
         #endregion
 
         private libProChic.ExplorerListView elvDesktop;
+        private System.Windows.Forms.Panel panTaskBar;
     }
 }
 
--- ProChic4.8/Desktop.cs
+++ ProChic4.8/Desktop.cs
@@ -19,14 +19,63 @@ namespace ProChic4._8
             try
             {
                 InitializeComponent();
+                CheckForIllegalCrossThreadCalls = false;
+                this.com.Config.ConfigUpdated += DesktopLoad;
+                this.Load += DesktopLoad;
 
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-            Console.WriteLine(elvDesktop.Items.Count + "");
+            elvDesktop.OSIconLocationPath = com.toSystemPath(com.Config.GetConfig("windows", "ICO").Setting);
             elvDesktop.BackColor = com.convertColour(com.Config.GetConfig("Colors", "Background").Setting);
+            elvDesktop.Directory = com.toSystemPath(@"C:\Windows\Desktop");
+        }
+        public void DesktopLoad(Object sender, EventArgs e)
+        {
+            try
+            {
+                elvDesktop.UpdateDesktop = false;
+                if (com.Config.GetConfig("Desktop", "TileWallpaper").Setting == "1") elvDesktop.WallpaperLayout = ImageLayout.Tile; else elvDesktop.WallpaperLayout = ImageLayout.Center;
+                elvDesktop.Wallpaper = (Bitmap)com.prepareImage(com.toSystemPath(com.Config.GetConfig("Desktop", "Wallpaper").Setting));
+                elvDesktop.Pattern = new Bitmap(getPattern(com.Config.GetConfig("Desktop", "Pattern").Setting), 8, 8);
+                int taskWidth=int.Parse(com.Config.GetConfig("Taskbar", "Width").Setting), taskHeight= int.Parse(com.Config.GetConfig("Taskbar", "Height").Setting);
+                panTaskBar.Height= taskHeight;
+                if (taskWidth == -1)
+                {
+                    panTaskBar.Dock = DockStyle.Bottom;
+                }else
+                {
+                    panTaskBar.Dock = DockStyle.None;
+                    panTaskBar.Width = taskWidth;
+                }
+                panTaskBar.Location = new Point((int)((Width/(Double)2)-(taskWidth/(Double)2)), Height - taskHeight);
+                elvDesktop.UpdateDesktop = true;
+            }
+            catch (Exception ex){
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        public Image getPattern(string patternString)
+        {
+            int y = 0;
+            System.Drawing.Bitmap canvasbitmap = new System.Drawing.Bitmap(152, 152);
+            Graphics g = Graphics.FromImage(canvasbitmap);
+            if (patternString == "(None)")
+                return canvasbitmap;
+            foreach (var dec in patternString.Split(' '))
+            {
+                int x = 0;
+                for (int index = 0; index <= 7; index++)
+                {
+                    if (Convert.ToString(Convert.ToInt32(dec), 2).PadLeft(8, '0').Substring(index, 1) == "1")
+                        g.FillRectangle(Brushes.Black, x * 19, y * 19, 19, 19);
+                    x += 1;
+                }
+                y += 1;
+            }
+            return canvasbitmap;
         }
     }
 }
