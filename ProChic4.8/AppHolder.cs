7dd43457519f05b0650e6c621d33d4a61c59393a
Luke S <Luke@LUKE-ACER>
2021-02-17 22:43:41 -0500

    Added support for closing, minimising, app scaling and app ico and Taskbar class (maybe moved later)

--- ProChic4.8/AppHolder.cs
+++ ProChic4.8/AppHolder.cs
@@ -31,15 +31,21 @@ namespace ProChic4._8
                 info.RedirectStandardInput = false;
                 info.RedirectStandardOutput = true;
                 info.RedirectStandardError = false;
+                info.WindowStyle = ProcessWindowStyle.Hidden;
                 pro = Process.Start(info);
                 pro.WaitForInputIdle();
+                pro.Exited += Exited;
                 while (pro.MainWindowHandle == null || pro.MainWindowHandle == IntPtr.Zero) ;
                 SetParent(pro.MainWindowHandle, GuestArea.Handle);
                 do {
                     GetClientRect(pro.MainWindowHandle, ref siz);
                 } while (siz.Width == 0 && siz.Height == 0);
+                MoveWindow(pro.MainWindowHandle, 0, 0, siz.Width, siz.Height, true);
                 Size = new Size(siz.Width + lefts.Width + rights.Width, siz.Height + programtopbar.Height + bottoms.Height + 0);
                 this.Maximise += Max;
+                this.DoubleBuffered = true;
+                base.Resize += resizing;
+                this.Closing += Exited;
                 //info.WindowStyle = ProcessWindowStyle.Hidden
 
                 //    do {
@@ -55,22 +61,27 @@ namespace ProChic4._8
                 //Size = new Size(siz.Width + lefts.Width + rights.Width, siz.Height + programtopbar.Height + bottoms.Height + 0); //Graphics.FromHwnd(pro.MainWindowHandle).DpiX, Graphics.FromHwnd(pro.MainWindowHandle).DpiY)             REMOVE EXTRAS AND SEE IF IT WORKS
                 // SetWindowPos(pro.MainWindowHandle, 0, panGuestMod.PointToScreen(panGuestMod.Location).X, panGuestMod.PointToScreen(panGuestMod.Location).Y, panGuestMod.Width, panGuestMod.Height, 64) //MoveWindow(pro.MainWindowHandle, panGuestMod.PointToScreen(panGuestMod.Location).X, panGuestMod.PointToScreen(panGuestMod.Location).Y, panGuestMod.Width, panGuestMod.Height, true)
                 //   Console.WriteLine("Title")
-                //    Text = pro.MainWindowTitle;
-                //    TsudaKageyu.IconExtractor i = new TsudaKageyu.IconExtractor(pro.StartInfo.FileName);
+                Text = pro.MainWindowTitle;
+                TsudaKageyu.IconExtractor i = new TsudaKageyu.IconExtractor(pro.StartInfo.FileName);
                 // in
-                //if (i.Count > 0) programIcon.Image = new Bitmap(i.GetIcon(i.Count - 1).ToBitmap(), 16, 16); else programIcon.Image = new Bitmap(com.prepareImage(com.toSystemPath(com.Config.GetConfig("windows", "GenericAppIco").Setting)), 16, 16); // New TsudaKageyu.IconUtil //pro.StartInfo.FileName).ToBitmap
-                //this.Tag = pro.Id;
-                //this.AccessibleName = exePath;
-                //      !!!SetParent(pro.MainWindowHandle, GuestArea.Handle);
-                // pro.StartInfo.WindowStyle = ProcessWindowStyle.Normal
-                //  Console.WriteLine("Finished")
-                //timCmdBuild.Start();//    //look       Console.WriteLine(closebutton.Height)
-            }
+                if (i.Count > 0) programIcon.Image = new Bitmap(i.GetIcon(i.Count - 1).ToBitmap(), 16, 16); else programIcon.Image = new Bitmap(com.prepareImage(com.toSystemPath(com.Config.GetConfig("windows", "GenericAppIco").Setting)), 16, 16); // New TsudaKageyu.IconUtil //pro.StartInfo.FileName).ToBitmap
+                 }
             catch (Exception ex) {
                 Debug.WriteLine(ex.ToString());
             }
         }
-
+        private void Exited (Object sender, EventArgs e)
+        {
+            AppClose(pro.MainWindowHandle);
+            pro.Close();
+        }
+        public event AppHandler AppClose;
+        public delegate void AppHandler(IntPtr Apphandler);
+        public IntPtr ProcessHandle { get { return pro.MainWindowHandle; } }
+        private void resizing (Object sender, EventArgs e)
+        {
+            MoveWindow(pro.MainWindowHandle, 0, 0, Size.Width-lefts.Width-rights.Width, Size.Height-programtopbar.Height-bottoms.Height, true);
+        }
         public void Max(Object Sender, EventArgs e) {
             if (!maximize) {
                 this.mexlocation = this.Location.X;
@@ -125,8 +136,57 @@ namespace ProChic4._8
         public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
         [DllImport("user32.dll")]
         private static extern bool GetClientRect(IntPtr HWND, ref Rectangle LPRECT);
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
-
     }
+    public class Taskbar
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int FindWindow(string className, string windowText);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int ShowWindow(int hwnd, int command);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int FindWindowEx(int parentHandle, int childAfter, string className, int windowTitle);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int GetDesktopWindow();
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 1;
+
+        protected static int Handle
+        {
+            get
+            {
+                return FindWindow("Shell_TrayWnd", "");
+            }
+        }
+
+        protected static int HandleOfStartButton
+        {
+            get
+            {
+                int handleOfDesktop = GetDesktopWindow();
+                int handleOfStartButton = FindWindowEx(handleOfDesktop, 0, "button", 0);
+                return handleOfStartButton;
+            }
+        }
+
+        private Taskbar()
+        {
+            // hide ctor
+        }
+
+        public static void Show()
+        {
+            ShowWindow(Handle, SW_SHOW);
+            ShowWindow(HandleOfStartButton, SW_SHOW);
+        }
+
+        public static void Hide()
+        {
+            ShowWindow(Handle, SW_HIDE);
+            ShowWindow(HandleOfStartButton, SW_HIDE);
+        }
+    }   //Code from https://stackoverflow.com/questions/19022789/hide-taskbar-in-winforms-application/19024531#19024531
 }
