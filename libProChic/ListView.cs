f4316e1c272c81874b5c0633f721f425108a07bc
Luke S <Luke@LUKE-ACER>
2021-02-17 22:37:21 -0500

    Added try...catch  and safer way of retrieve img

--- libProChic/ListView.cs
+++ libProChic/ListView.cs
@@ -60,64 +60,70 @@ namespace libProChic
             components = new System.ComponentModel.Container();
                 OwnerDraw = true;
             DoubleBuffered = true;
-           SelectedColor = com.convertColour(com.Config.GetConfig("Colors", "Hilight").Setting);
-           SelectedTextColor = com.convertColour(com.Config.GetConfig("Colors", "HilightText").Setting);
-           BackColor = com.convertColour(com.Config.GetConfig("Colors", "Window").Setting);
-           ForeColor = com.convertColour(com.Config.GetConfig("Colors", "WindowText").Setting);
+            SelectedColor = com.convertColour(com.Config.GetConfig("Colors", "Hilight").Setting);
+            SelectedTextColor = com.convertColour(com.Config.GetConfig("Colors", "HilightText").Setting);
+            BackColor = com.convertColour(com.Config.GetConfig("Colors", "Window").Setting);
+            ForeColor = com.convertColour(com.Config.GetConfig("Colors", "WindowText").Setting);
             this.DrawItem += mybase_DrawItem;
             //this.DrawSubItem += mybase_DrawItem;
             GridLines = true;
         }
         private void mybase_DrawItem(object sender, DrawListViewItemEventArgs e)        //http://vb-helper.com/howto_net_owner_draw_listview.html
         {
-            //  e.DrawText();// = false;
-            // Draw Details view items in the DrawSubItem event handler.
-            //      if (e.Item.ListView.View == View.Details) return;
+            try
+            {
+                //  e.DrawText();// = false;
+                // Draw Details view items in the DrawSubItem event handler.
+                //      if (e.Item.ListView.View == View.Details) return;
 
-            // Get the ListView item and the ServerStatus object.
-            //      ListViewItem item = e.Item;
+                // Get the ListView item and the ServerStatus object.
+                //      ListViewItem item = e.Item;
 
-            // Clear.
-            //e.DrawBackground();
-            if (e.State == ListViewItemStates.Selected) ItemColour(e.ItemIndex, SelectedTextColor, SelectedColor); else ItemColour(e.ItemIndex, ForeColor, BackColor);
-            StringFormat sf = new StringFormat();
-            sf.Alignment = StringAlignment.Center;
-            sf.LineAlignment = StringAlignment.Center;
-            sf.FormatFlags = StringFormatFlags.DirectionRightToLeft;
+                // Clear.
+                //e.DrawBackground();
+                if (e.State == ListViewItemStates.Selected) ItemColour(e.ItemIndex, SelectedTextColor, SelectedColor); else ItemColour(e.ItemIndex, ForeColor, BackColor);
+                StringFormat sf = new StringFormat();
+                sf.Alignment = StringAlignment.Center;
+                sf.LineAlignment = StringAlignment.Center;
+                sf.FormatFlags = StringFormatFlags.DirectionRightToLeft;
 
-            // Draw a status indicator.
-            //        e.Graphics.SmoothingMode =
-            //            System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            Rectangle rect = new Rectangle(e.Bounds.Left + 1, e.Bounds.Top + 1,e.Bounds.Height - 2, e.Bounds.Height - 2);
-   //        using (SolidBrush br =
-   //            new SolidBrush(ForeColor))
-   //        {
-   //            e.Graphics.FillEllipse(br, rect);
-   //        }
-   //        e.Graphics.DrawEllipse(Pens.Black, rect);
-           int left = rect.Right + 2;
-            //
-            Bitmap img = new Bitmap(SmallImageList.Images.Get(e.Item.ImageIndex),ImageSize);
-            //         // See how much we must scale it.
-            //         float scale;
-            //  Console.WriteLine(e.Bounds.Height + ","+ ImageSize.Height);
-            float scale = 1F;// ImageSize.Height;//e.Bounds.Height;// / (float)ImageSize.Height;//img.Height;
+                // Draw a status indicator.
+                //        e.Graphics.SmoothingMode =
+                //            System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                Rectangle rect = new Rectangle(e.Bounds.Left + 1, e.Bounds.Top + 1,e.Bounds.Height - 2, e.Bounds.Height - 2);
+                //        using (SolidBrush br =
+                //            new SolidBrush(ForeColor))
+                //        {
+                //            e.Graphics.FillEllipse(br, rect);
+                //        }
+                //        e.Graphics.DrawEllipse(Pens.Black, rect);
+                int left = rect.Right + 2;
+                //
+                Bitmap img = new Bitmap(1, 1);
+                if (SmallImageList.Images.Get(e.Item.ImageIndex) != null) img = new Bitmap(SmallImageList.Images.Get(e.Item.ImageIndex), ImageSize);
+                //         // See how much we must scale it.
+                //         float scale;
+                //  Console.WriteLine(e.Bounds.Height + ","+ ImageSize.Height);
+                float scale = 1F;// ImageSize.Height;//e.Bounds.Height;// / (float)ImageSize.Height;//img.Height;
 
-            // Scale and position the image.
-            e.Graphics.ScaleTransform(scale, scale);
-            e.Graphics.TranslateTransform(left,left+(e.ItemIndex*((ImageSize.Height+30)*scale)) ,System.Drawing.Drawing2D.MatrixOrder.Append);  //+ (e.Bounds.Height - img.Height * scale) /1
-            //
-            //         // Draw the image.
-            e.Graphics.DrawImage(com.prepareImage(img, true), 0, 0);
-            SizeF sizTxt = e.Graphics.MeasureString(e.Item.Text, e.Item.Font, e.Bounds.Width, sf);
-            Rectangle txtRec = new Rectangle(new Point(0, img.Height), new Size((int)sizTxt.Width,(int)sizTxt.Height));//TextRenderer.MeasureText(e.Item.Text, e.Item.Font, new Size(img.Width, img.Height)));
-            
-            e.Graphics.FillRectangle(new SolidBrush(e.Item.BackColor),txtRec);
-            e.Graphics.DrawString(e.Item.Text, e.Item.Font, new SolidBrush(e.Item.ForeColor), txtRec,sf);
-            // Draw the focus rectangle if appropriate.
-           e.Graphics.ResetTransform();
-          //  e.DrawFocusRectangle();
-        
+                // Scale and position the image.
+                e.Graphics.ScaleTransform(scale, scale);
+                e.Graphics.TranslateTransform(left,left+(e.ItemIndex*((ImageSize.Height+30)*scale)) ,System.Drawing.Drawing2D.MatrixOrder.Append);  //+ (e.Bounds.Height - img.Height * scale) /1
+                                                                                                                                                             //
+                                                                                                                                                             //         // Draw the image.
+                e.Graphics.DrawImage(com.prepareImage(img, true), 0, 0);
+                SizeF sizTxt = e.Graphics.MeasureString(e.Item.Text, e.Item.Font, e.Bounds.Width, sf);
+                Rectangle txtRec = new Rectangle(new Point(0, img.Height), new Size((int)sizTxt.Width,(int)sizTxt.Height));//TextRenderer.MeasureText(e.Item.Text, e.Item.Font, new Size(img.Width, img.Height)));
+
+                e.Graphics.FillRectangle(new SolidBrush(e.Item.BackColor),txtRec);
+                e.Graphics.DrawString(e.Item.Text, e.Item.Font, new SolidBrush(e.Item.ForeColor), txtRec,sf);
+                // Draw the focus rectangle if appropriate.
+                e.Graphics.ResetTransform();
+                //  e.DrawFocusRectangle();
+            }catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
     }
         private void ItemColour(int index, Color fColour, Color bColour)
         {
