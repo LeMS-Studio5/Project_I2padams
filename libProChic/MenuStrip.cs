23c81bd8545b4c32381951569043226eb3a70d53
Luke S <Luke@LUKE-ACER>
2021-07-06 19:38:01 -0400

    Added Backkground colour support Last Edited Mar 2021

--- libProChic/MenuStrip.cs
+++ libProChic/MenuStrip.cs
@@ -16,7 +16,14 @@ namespace libProChic
     }
     public class ProChicCol : ProfessionalColorTable
     {
+        Color bkgrdCol;
+        Color bkgrdSelCol;
         MasterClass com = new MasterClass();
+        public ProChicCol()
+        {
+            bkgrdSelCol = com.convertColour(com.Config.GetConfig("Colors", "Hilight").Setting);
+            bkgrdCol = com.convertColour(com.Config.GetConfig("Colors", "ButtonFace").Setting);
+        }
         public override Color MenuItemSelected
         {
             get { return com.convertColour(com.Config.GetConfig("Colors", "Hilight").Setting); }
@@ -25,50 +32,83 @@ namespace libProChic
         {
             //base.Presse
         }
-        //public override Color MenuItemSelectedGradientEnd { get { return com.convertColour(com.Config.GetConfig("Colors", "Hilight").Setting); } }
-        //public override Color MenuStripGradientEnd{ get { return com.convertColour(com.Config.GetConfig("Colors", "Hilight").Setting); } }
-        //public override Color MenuItemSelectedGradientBegin
-        //{
-        //    get { return Color.Orange; }
-        //}
-        //public override Color MenuItemSelectedGradientEnd
-        //{
-        //    get { return Color.Yellow; }
-        //}
-        public override Color MenuItemSelectedGradientBegin
-        {
-            get
-            {
-                return Color.Navy;
+        public override Color ToolStripDropDownBackground{
+            get  {
+                return bkgrdCol;
             }
         }
-
+            public override Color ImageMarginGradientBegin{
+                get {
+                    return bkgrdCol;
+                }
+        }
+        public override Color ImageMarginGradientMiddle{
+            get {
+                return bkgrdCol;
+            }
+        }
+        public override Color ImageMarginGradientEnd{
+            get {
+                return bkgrdCol;
+            }
+        }
+        public override Color MenuBorder{
+            get  {
+                return Color.Black;
+            }
+        }
+        public override Color MenuItemBorder{
+            get {
+                return Color.Black;
+            }
+        }
+        public override Color MenuStripGradientBegin {
+            get {
+                return bkgrdCol;
+            }
+        }
+        public override Color MenuStripGradientEnd {
+            get{
+                return bkgrdCol;
+            }
+        }
+        public override Color MenuItemSelectedGradientBegin {
+            get{
+                return bkgrdSelCol; 
+            }
+        }
         public override Color MenuItemSelectedGradientEnd
         {
-            get
-            {
-                return Color.Navy;
+            get{
+                return bkgrdSelCol;
+            }
+        }
+        public override Color MenuItemPressedGradientBegin
+        {
+            get{
+                return bkgrdSelCol;
+            }
+        }
+        public override Color MenuItemPressedGradientEnd {
+            get{
+                //if (e.Item.Selected) return ; else e.Item.ForeColor = com.convertColour(com.Config.GetConfig("Colors", "MenuText").Setting);
+                return bkgrdSelCol;
             }
         }
     }
     public class ProChicRender : ToolStripProfessionalRenderer
     {
-        Color bkgrdCol;
-        Color bkgrdSelCol;
         MasterClass com = new MasterClass();
         public ProChicRender() : base(new ProChicCol())
         {
-            bkgrdSelCol = com.convertColour(com.Config.GetConfig("Colors", "Hilight").Setting);
-            bkgrdCol = com.convertColour(com.Config.GetConfig("Colors", "ButtonFace").Setting);
         }
         protected override void OnRenderMenuItemBackground(ToolStripItemRenderEventArgs e)
         {
-            //base.OnRenderMenuItemBackground(e);
+            base.OnRenderMenuItemBackground(e);
             Rectangle rc = new Rectangle(Point.Empty, e.Item.Size);
             Color c = Color.PaleGreen;
-            if (e.Item.Selected) c = bkgrdSelCol; else c = bkgrdCol;
-            using (SolidBrush brush = new SolidBrush(c))
-                e.Graphics.FillRectangle(brush, rc);
+            //if (e.Item.Selected) c = bkgrdSelCol; else c = bkgrdCol;
+            //using (SolidBrush brush = new SolidBrush(c))   e.Graphics.FillRectangle(brush, rc);
             //e.ToolStrip.BackColor = Color.Yellow;//e.Item.BackColor = Color.Yellow
             //if (e.Item.Selected) e.ToolStrip.Col= Color.Yellow;  else e.Item.BackColor = bkgrdCol;
             ////  e.Item.BackColor= com.convertColour(com.Config.GetConfig("Colors", "Hilight").Setting);
